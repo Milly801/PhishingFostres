4c5a182208d04f78dc39db585a6b45a8c48844b7
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2025-04-20 23:08:05 +0000

    user model and repository stabilized

--- server/repositories/user_repository.py
+++ server/repositories/user_repository.py
@@ -1,9 +1,9 @@
 from sqlalchemy.exc import NoResultFound
-from server.models.user_model import User
-from server.models.db_config import ph_session
+from models.user_model import User
+from models.db_config import ph_session
 
 class UserRepository:
-    def __init__(self, db_session=ph_session): 
+    def __init__(self, db_session=ph_session):
         self.db_session = db_session
 
     def create_user(self, user_data: dict):
@@ -12,21 +12,24 @@ class UserRepository:
         self.db_session.commit()
         return new_user
 
-    def get_user_by_id(self, user_id: int):
+    def get_all_users(self):
+        return self.db_session.query(User).all()
+
+    def get_user_by_id(self, user_id: str):
         try:
-            user = self.db_session.query(User).filter_by(id=user_id).one()
+            user = self.db_session.query(User).filter_by(id=user_id).first()
             return user
         except NoResultFound:
             return None
 
     def get_user_by_email(self, email: str):
         try:
-            user = self.db_session.query(User).filter_by(email=email).one()
+            user = self.db_session.query(User).filter_by(email=email).first()
             return user
         except NoResultFound:
             return None
 
-    def update_user(self, user_id: int, updated_data: dict):
+    def update_user(self, user_id: str, updated_data: dict):
         user = self.get_user_by_id(user_id)
         if user:
             for key, value in updated_data.items():
@@ -35,10 +38,13 @@ class UserRepository:
             return user
         return None
 
-    def delete_user(self, user_id: int):
+    def delete_user(self, user_id: str):
+        print(f"Attempting to delete user with ID: {user_id}")
         user = self.get_user_by_id(user_id)
         if user:
+            print(f"User found: {user}")
             self.db_session.delete(user)
             self.db_session.commit()
+            print(f"User deleted successfully.")
             return True
         return False
