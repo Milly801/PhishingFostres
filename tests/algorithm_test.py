555acdaf61a117ee1f327add759e4b20beb2c2a8
memoe <memoemukoe@gmail.com>
2025-05-01 14:47:27 +0200

    compare algorithm

---
+++ tests/algorithm_test.py
@@ -0,0 +1,13 @@
+import pytest
+from sqlalchemy import create_engine
+from server.models.db_config import ph_session
+from server.models.base import Base
+from server.services.compareAlgorithm import compare_user_response
+
+
+email_id = "3b715dcf-0699-43c6-8e45-9487f2d17196"  
+user_response = "Yes"  
+
+
+result = compare_user_response(email_id, user_response)
+print(result)
